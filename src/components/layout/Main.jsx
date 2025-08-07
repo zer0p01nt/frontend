7839db64bd77d8a434b063e4e7c80e25c6fcb924
zer0p01nt <1027.bmy@gmail.com>
2025-08-07 19:37:11 +0900

    🎨 Design: width 고정 레이아웃 생성

---
+++ src/components/layout/Main.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+/* 모바일 환경 구현을 위해 max-width를 393px로 고정 */
+const MainWrapper = styled.div`
+  width: 100%;
+  max-width: 393px;
+  margin: 0 auto;
+  min-height: 100vh;
+  background-color: #ffffff;
+`;
+
+export default function Main({ children }) {
+  return <MainWrapper>{children}</MainWrapper>;
+}
