b10e5a207bc3d7bc968f16913cbd4f6ad6de3032
zer0p01nt <1027.bmy@gmail.com>
2025-08-10 03:20:04 +0900

    ✨ Feat: GoToTop UI 및 기능 구현

---
+++ src/components/GoToTop/GoToTop.jsx
@@ -0,0 +1,34 @@
+import * as B from "../../styles/ButtonCircle";
+import arrow from "../../assets/arrow_upward.svg";
+import { useEffect, useState } from "react";
+
+export default function GoToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsVisible(window.pageYOffset > 200 ? true : false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    // 실제로 사용할 땐 꼭 ButtonWrapper로 감싸주세요
+    // <B.ButtonWrapper>
+    <B.ButtonCircle
+      icon={arrow}
+      type='button'
+      onClick={scrollToTop}
+      $isVisible={isVisible} // 경고문 안 뜨도록 $ 붙임
+    />
+    // </B.ButtonWrapper>
+  );
+}
