6844d9e36e44f47a689a7594be4633c94628be61
zer0p01nt <1027.bmy@gmail.com>
2025-08-12 19:34:19 +0900

    ✨ Feat: value 관련 로직 추가

--- src/components/ChatbotInputField/ChatbotInputField.jsx
+++ src/components/ChatbotInputField/ChatbotInputField.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import * as S from "./ChatbotInputFieldStyle";
 
-export default function ChatbotInputField({ $isLoading }) {
+export default function ChatbotInputField({ $isLoading, onSubmit }) {
   const defaultPlaceholder = "궁금한 건 뭐든 물어보세요"
   const [placeholder, setPlaceholder] = useState(defaultPlaceholder);
-  // const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
   // focus 시 placeholder
   const handleFocus = () => {
@@ -18,12 +18,18 @@ export default function ChatbotInputField({ $isLoading }) {
     }
   };
 
-  // value 관련 로직 추가 필요
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const msg = value.trim();
+    if (!msg || $isLoading) return;
+    onSubmit?.(msg);
+    setValue("")
+  }
 
   return (
-    <S.ChatbotInputForm>
-      <S.ChatbotInputField placeholder={$isLoading ? "정보를 정리하고 있어요..." : placeholder} disabled={$isLoading} onFocus={handleFocus} onBlur={handleBlur} />
-      <S.ChatbotBtn $isLoading={$isLoading}></S.ChatbotBtn>
+    <S.ChatbotInputForm onSubmit={handleSubmit}>
+      <S.ChatbotInputField placeholder={$isLoading ? "정보를 정리하고 있어요..." : placeholder} disabled={$isLoading} onChange={(e) => setValue(e.target.value)} onFocus={handleFocus} onBlur={handleBlur} value={value} required />
+      <S.ChatbotBtn type="submit" $isLoading={$isLoading}></S.ChatbotBtn>
     </S.ChatbotInputForm>
   );
 }
