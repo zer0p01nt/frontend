533597a873eaf4e56ec13155174acb4aaac7e8f0
zer0p01nt <1027.bmy@gmail.com>
2025-08-14 05:20:08 +0900

    ✨ Feat: 마이페이지 관련 경로 추가

---
+++ src/pages/MyPage/MyPageStyle.jsx
@@ -0,0 +1,99 @@
+import styled from "styled-components";
+
+// 제목 부분 전체를 감싸는 컨테이너
+export const TitleContainer = styled.div`
+  background: linear-gradient(
+    180deg,
+    var(--color-blue-50) 85.04%,
+    var(--color-pink-50) 111.61%
+  );
+  // 전체 컨테이너의 padding 무시하고 배경 덮기
+  margin: 0 -16px;
+  // 헤더까지 배경 덮기
+  padding: 66px 24px 24px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+export const Character = styled.div`
+  width: 80px;
+  height: 90px;
+  background: #d9d9d9;
+`;
+
+export const TitleBox = styled.div`
+  display: flex;
+  padding: 0 8px;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 4px;
+`;
+
+export const Username = styled.div`
+  color: var(--color-base-black);
+  font-size: var(--Heading-lg-font-size);
+  font-weight: 700;
+  line-height: var(--Heading-lg-line-height);
+  letter-spacing: var(--letter-spacing);
+`;
+
+export const Text = styled.span`
+  color: var(--color-base-black);
+  font-size: var(--Heading-sm-font-size);
+  font-weight: 600;
+  line-height: var(--Heading-sm-line-height);
+  letter-spacing: var(--letter-spacing);
+
+  strong {
+    color: var(--color-neutral-brand-primary);
+    font-weight: 600;
+  }
+`;
+
+export const BadgeWrapper = styled.div`
+  display: flex;
+  padding: 12px;
+  align-items: flex-start;
+  gap: 8px;
+  border-radius: var(--border-radius-2xl);
+  border: 1px solid var(--color-blue-50);
+  background: var(--color-base-white);
+  flex-wrap: wrap;
+`;
+
+// 뱃지 색이 기존 컴포넌트와 달라 임의로 추가했습니다
+export const MyPageBadge = styled.span`
+  display: inline-flex;
+  padding: 4px 12px;
+  justify-content: center;
+  align-items: center;
+  border-radius: var(--border-radius-rounded);
+  font-size: var(--Body-sm-font-size);
+  font-weight: 600;
+  line-height: var(--Body-sm-line-height);
+  background-color: ${({ $variant }) =>
+    $variant === "region" ? "var(--color-blue-50)" : "var(--color-teal-50)"};
+  color: ${({ $variant }) =>
+    $variant === "region"
+      ? "var(--color-blue-400-main)"
+      : "var(--color-teal-600)"};
+  flex-shrink: 0;
+`;
+
+export const SectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
---
+++ src/pages/ScrapedPosts/ScrapedPosts.jsx
@@ -0,0 +1,5 @@
+import * as S from "./ScrapedPostsStyle";
+
+export default function ScrapedPosts() {
+  return <div>스크랩한 공문 페이지</div>;
+}
