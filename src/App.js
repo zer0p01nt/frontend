2761e01e8b4dee64f829cfc3d4bdcab4a1abb095
zer0p01nt <1027.bmy@gmail.com>
2025-08-14 15:30:31 +0900

    ✏️ Typo: 주석 위치 이동

--- src/App.js
+++ src/App.js
@@ -39,8 +39,8 @@ function App() {
           <Route element={<WithoutNav />}>
             {/* Navigator가 없는 경우 이쪽에 route를 만들어주세요 */}
             <Route path='/detail' element={<Detail />} />
-            <Route path='/search' element={<Search />} />
             {/* 추후 /post/:id로 경로 변경할 예정 */}
+            <Route path='/search' element={<Search />} />
             <Route path='/profile' element={<Profile />} />
             <Route path='/scrap/posts' element={<ScrapedPosts />} />
             <Route path='/scrap/chatbots' element={<ScrapedChatbots />} />
