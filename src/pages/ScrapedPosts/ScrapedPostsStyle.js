da6c19657f812a728f55f4b75686149a221be21e
zer0p01nt <1027.bmy@gmail.com>
2025-08-15 20:00:56 +0900

    ✨ Feat: 공문 스크랩 페이지 UI 구현

--- src/pages/ScrapedPosts/ScrapedPostsStyle.js
+++ src/pages/ScrapedPosts/ScrapedPostsStyle.js
@@ -7,3 +7,27 @@ export const ScrapedContainer = styled.div`
   max-width: 393px;
   box-sizing: border-box;
 `;
+
+export const OrderContainer = styled.div`
+  padding: 0 24px;
+`;
+
+export const ResultBox = styled.div`
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  color: var(--color-neutral-600);
+  font-size: var(--Body-sm-font-size);
+  font-weight: 500;
+  line-height: var(--Body-sm-line-height);
+  letter-spacing: -0.1px;
+`;
+
+export const OrderBtn = styled.button`
+  display: flex;
+  padding: 8px 0;
+  align-items: center;
+  gap: 5px;
+  flex-wrap: nowrap;
+  cursor: pointer;
+`;
