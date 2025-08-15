009748961d37ed05da12361cbea360ff3d6019fe
zer0p01nt <1027.bmy@gmail.com>
2025-08-15 19:23:13 +0900

    🎨 Design: 공문 스크랩 페이지 중간 저장

--- src/pages/ScrapedPosts/ScrapedPostsStyle.js
+++ src/pages/ScrapedPosts/ScrapedPostsStyle.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ScrapedContainer = styled.div`
   margin: 0 auto;
-  margin-top: 42px;
+  padding-top: 42px;
   width: 100%;
   max-width: 393px;
   box-sizing: border-box;
