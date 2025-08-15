0d47985993de1cf8025da1fc08f0d31c9b6f5b86
zer0p01nt <1027.bmy@gmail.com>
2025-08-15 17:01:36 +0900

    🎨 Design: 공문 스크랩 페이지 중간 저장

--- src/pages/ScrapedPosts/ScrapedPosts.jsx
+++ src/pages/ScrapedPosts/ScrapedPosts.jsx
@@ -1,5 +1,26 @@
+import GoToTop from "../../components/GoToTop/GoToTop";
+import Header from "../../components/Header/Header";
+import { ButtonWrapper } from "../../styles/ButtonCircle";
 import * as S from "./ScrapedPostsStyle";
+import Filter from "../../components/Filter/Filter";
+import SmallFilter from "../../components/SmallFilter/SmallFilter";
+import { SmallFilterWrapper } from "../../components/SmallFilter/SmallFilterStyle";
 
 export default function ScrapedPosts() {
-  return <div>스크랩한 공문 페이지</div>;
+  return (
+    <>
+      {/* fixed 되는 컴포넌트들 */}
+      <Header hasBack={true} title='공문 스크랩' hasScrap={false} />
+      <ButtonWrapper>
+        <GoToTop />
+      </ButtonWrapper>
+      <S.ScrapedContainer>
+        <Filter />
+        <SmallFilterWrapper>
+          <SmallFilter sourceKey='user_regions' />
+          <SmallFilter sourceKey='user_categories' />
+        </SmallFilterWrapper>
+      </S.ScrapedContainer>
+    </>
+  );
 }
---
+++ src/pages/ScrapedPosts/ScrapedPostsStyle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ScrapedContainer = styled.div`
+  margin: 0 auto;
+  margin-top: 42px;
+  width: 100%;
+  max-width: 393px;
+  box-sizing: border-box;
+`;
