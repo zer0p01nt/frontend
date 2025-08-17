46ee302c2564c4f3fd945155efa393bda34fd74d
zer0p01nt <1027.bmy@gmail.com>
2025-08-17 22:14:06 +0900

    ✨ Feat: 카테고리, 지역명 매핑 파일 추가

---
+++ src/services/maps.js
@@ -0,0 +1,24 @@
+// CATEGORY_OPTIONS 인덱스로 갖다 써도 될 것 같지만 일단 필요할 것 같아서 적어둡니다
+export const CATEGORY_MAP = {
+  0: "모든 주제",
+  1: "교통",
+  2: "문화",
+  3: "주택",
+  4: "경제",
+  5: "환경",
+  6: "안전",
+  7: "복지",
+  8: "행정",
+};
+
+// id로 지역 매핑
+export const REGION_MAP = {
+  6: "도봉구",
+  85: "경기도",
+};
+
+// 지역명으로 id 매핑
+export const NAME_REGION_MAP = {
+  도봉구: 6,
+  경기도: 85,
+};
