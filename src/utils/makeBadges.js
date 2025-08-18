3034015d579cd156783dbc4a80e275bc0365cd2b
zer0p01nt <1027.bmy@gmail.com>
2025-08-19 01:54:57 +0900

    ✨ Feat: 스크랩 목록 뱃지 로직 추가

--- src/utils/makeBadges.js
+++ src/utils/makeBadges.js
@@ -10,3 +10,15 @@ export const makeBadges = (r) => [
     color: "teal",
   })),
 ];
+
+// 스크랩 목록 조회 API 응답 데이터에 맞춰 수정
+export const makeScrapBadges = (r) => [
+  {
+    text: REGION_MAP[r.region.id],
+    color: "blue",
+  },
+  ...(r.categories ?? []).map((c) => ({
+    text: c.category_name,
+    color: "teal",
+  })),
+];
