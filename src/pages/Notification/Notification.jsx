a6631761ed44c76fdee1da8f8246730f050b3b16
zer0p01nt <1027.bmy@gmail.com>
2025-08-21 16:43:26 +0900

    🐛 Fix: 알림 목록 데이터 가져오기 수정

--- src/pages/Notification/Notification.jsx
+++ src/pages/Notification/Notification.jsx
@@ -9,7 +9,7 @@ import * as S from "./NotificationStyle";
 import * as F from "../../components/SmallFilter/SmallFilterStyle";
 import useFetch from "../../hooks/useFetch";
 import NotificationEmpty from "./NotificationEmpty";
-import { makeScrapBadges } from "../../utils/makeBadges"; // 뱃지 생성 유틸리티
+import { makeNotiBadges } from "../../utils/makeBadges"; // 뱃지 생성 유틸리티
 import { useNavigate } from "react-router-dom"; // 페이지 이동 훅
 import {
   CATEGORY_TYPE_MAP,
@@ -73,7 +73,9 @@ export default function Notification() {
   );
 
   // 실제 알림 목록은 API 응답의 'results' 안에 들어있습니다.
-  const notifications = notificationData?.results ?? [];
+  const notifications = notificationData?.data?.results ?? [];
+
+  console.log(notifications);
 
   return (
     <>
@@ -99,13 +101,13 @@ export default function Notification() {
               <CardList
                 key={item.id}
                 variant='notification'
-                badges={makeScrapBadges(item)} // API 데이터에 맞게 뱃지 생성
-                title={item.doc_title} // 실제 제목 데이터
-                date={item.pub_date.slice(0, 10)} // 실제 날짜 데이터
+                badges={makeNotiBadges(item)} // API 데이터에 맞게 뱃지 생성
+                title={item.title} // 실제 제목 데이터
+                date={item.notification_time.slice(0, 10)} // 실제 날짜 데이터
                 isUnread={!item.is_read} // '읽음' 상태의 반대로 '안읽음' 표시
-                onClick={() => navigate(`/post/${item.id}`)} // 클릭 시 상세 페이지로 이동
-                image={item.image_url}
-                type={item.doc_type}
+                onClick={() => navigate(`/post/${item.document_info.id}`)} // 클릭 시 상세 페이지로 이동
+                image={item.document_info.image_url}
+                type={item.document_info.doc_type}
               />
             ))
           ) : (
