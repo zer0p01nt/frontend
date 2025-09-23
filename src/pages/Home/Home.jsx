759ea4767d1518162a90a6d41a1f6f867e345021
zer0p01nt <1027.bmy@gmail.com>
2025-09-23 06:24:21 +0000

    �Docs: 영상 촬영용 MockData 추가

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,6 +12,48 @@ import { NAME_REGION_MAP } from "../../constants/maps.js";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
+// 더미데이터 추가
+const MOCKDATA = [
+  {
+    id: 2036,
+    doc_title: "도봉구 청년 경제특강 참여자 모집",
+    doc_content:
+      "도봉구 청년 및 관심있는 구민을 대상으로「도봉구 청년 경제특강」을 개최합니다.경제 전문가'슈카'를 초청하여 청년들의 경제적 자립을 위한 투자 전략 등의 내용 으로 경제 특강을 진행하오니 관심있는 분들의 많은 참여 부탁드립니다.*특 강 명:큰 거 온다.. '슈카'와 함께하는 경제 특강*특강일시:2025. 9. 18.(목) 15:00*장 소:도봉구청2층 선인봉",
+    doc_type: "PARTICIPATION",
+    doc_type_display: "참여",
+    pub_date: "2025-08-21T21:23:00+09:00",
+    dead_date: "2025-10-18T00:00:00+09:00",
+    has_deadline: true,
+    categories: [
+      {
+        id: 4,
+        category_name: "경제",
+      },
+    ],
+    region_id: 6,
+    image_url: null,
+  },
+  {
+    id: 2040,
+    doc_title: "2025 반려식물 체험 프로그램 수강생 모집 안내",
+    doc_content:
+      "2025반려식물 체험 프로그램 수강생 모집 안내구민들에게 정서적 안정과 건강 증진에 기여하고자 도봉구민 대상으로“반려식물 체험프로그램”수강생을 다음과 같이 모집합니다.1.교 육 명: 2025년 반려식물 체험 프로그램2.대 상:도봉구 주민3.신청기간:2025. 8. 19.(화) 10:00∼마감시까지4.교육기간:2025. 9. 10. ~ 11. 19. (매주",
+    doc_type: "PARTICIPATION",
+    doc_type_display: "참여",
+    pub_date: "2025-08-21T14:03:00+09:00",
+    dead_date: "2025-10-19T00:00:00+09:00",
+    has_deadline: true,
+    categories: [
+      {
+        id: 8,
+        category_name: "행정",
+      },
+    ],
+    region_id: 6,
+    image_url: null,
+  },
+];
+
 export default function Home() {
   const navigate = useNavigate();
   const goToSearch = () => {
@@ -165,7 +207,8 @@ export default function Home() {
               />
             </S.SectionHeader>
             <S.HorizontalScrollWrapper>
-              {!isScrapedPostsLoading &&
+              {/* 더미데이터 주입을 위한 주석처리 */}
+              {/* {!isScrapedPostsLoading &&
                 scrapedPosts.map((item) => (
                   <CardList
                     key={item.document.id}
@@ -176,7 +219,18 @@ export default function Home() {
                     image={item.document.image_url}
                     type={item.document.doc_type}
                   />
-                ))}
+                ))} */}
+              {MOCKDATA.map((item) => (
+                <CardList
+                  key={item.id}
+                  variant='card'
+                  badges={makeScrapBadges(item)}
+                  title={item.doc_title}
+                  onClick={() => navigate(`/post/${item.id}`)}
+                  image={item.image_url}
+                  type={item.doc_type}
+                />
+              ))}
             </S.HorizontalScrollWrapper>
           </div>
 
